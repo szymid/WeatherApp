ca213bd538a07e3dc3de49a5ce940547daff56b8
Szymon Dawidow <szymid@interia.pl>
2016-05-08 12:38:16 +0200

    Initial Commit

---
+++ WeatherApp_wakeUp WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  WeatherApp_wakeUp WatchKit Extension
+//
+//  Created by Boguslaw Dawidow on 08.05.2016.
+//  Copyright © 2016 Szymon Dawidow. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
